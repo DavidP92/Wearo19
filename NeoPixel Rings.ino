79eb8f96795e3f4865a2b87780f38351ccff874b
MaSanto Raffiki <Davidpaez1006@gmail.com>
2019-02-28 16:33:43 -0700

    Update NeoPixel Rings.ino

--- NeoPixel Rings.ino
+++ NeoPixel Rings.ino
@@ -3,21 +3,23 @@
   #include <avr/power.h>
 #endif
  
-#define bigO 6
-#define medO 9
+#define bigO A1
+#define medO A2
 #define lilO 3
 
 #define ledM 16
-#define ledB 24
+#define ledB 30
 #define ledS 12
+#define MBRIGHTNESS 175
+#define BBRIGHTNESS 150
 
-#define MBRIGHTNESS 30
-#define BBRIGHTNESS 50
+#define PIN A0
+#define N_LEDS 30
 
 const uint8_t clear_led[4] = {255, 255, 255, 255};
 const uint8_t clear_bright = 0;
 
-#define MAXBRIGHTNESS 30
+#define MAXBRIGHTNESS 230
 #define MINBRIGHTNESS 0
 uint8_t modBrightness = 0;
 bool brightnessDir = false;
@@ -25,45 +27,82 @@ bool brightnessDir = false;
 Adafruit_NeoPixel strip0 = Adafruit_NeoPixel(ledB, bigO, NEO_RGBW + NEO_KHZ800);
 Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(ledM, medO, NEO_RGBW + NEO_KHZ800);
 Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(ledS, lilO, NEO_RGBW + NEO_KHZ800);
+Adafruit_NeoPixel strip3 = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+
  
 void setup() {
   setupPixels(&strip0);
   setupPixels(&strip1);
   setupPixels(&strip2);
+  setupPixels(&strip3);
   Serial.begin(9600);
 }
  
 void loop() {
   // Some example procedures showing how to display to the pixels:
-  uint8_t a[] = {1, 1, 255, 50};   //Designate Color of strip0
-  uint8_t b[] = {1,255,1,25}; //Designate Color of strip1
-  uint8_t c[] = {255,1,1,15};
+  uint8_t a[] = {1, 255, 1, 5};   //Designate Color-------> Red
+  uint8_t b[] = {1,1,255,1}; //Designate Color -----> Blue
+  uint8_t c[] = {255,1,1};
+  uint8_t d[] = {1,1,255};
+  uint8_t e[] = {255,255,255};
   delay(50);
+
+  Serial.println("[1]");
+  setPColor(&strip0, a, 75); setPColor(&strip1, b, 75); setPColor(&strip2,c,75); setPColor(&strip3,c,175);
+  delay(2500);
   
-  setPColor(&strip0, a, 50); setPColor(&strip1, b, 50); setPColor(&strip2,c,50);
-  delay(1500);
-  
+  Serial.println("[2]");
   setPColor(&strip0, clear_led, clear_bright);  //Turn Off
   setPColor(&strip1,clear_led,clear_bright);  // Turn Off
   setPColor(&strip2,clear_led,clear_bright);
+  setPColor(&strip3,clear_led,clear_bright);
 
-  setPColor(&strip0, b, 50); setPColor(&strip1, c, 50); setPColor(&strip2,a,50);
-  delay(1500);  
+  Serial.println("[3]");
+  setPColor(&strip0, b, 50); setPColor(&strip1, a, 50); setPColor(&strip2,a,50);setPColor(&strip3,d,255);
+  delay(2500);  
   
+  Serial.println("[4]");
   setPColor(&strip0, clear_led, clear_bright);  //Turn Off
   setPColor(&strip1,clear_led,clear_bright);  // Turn Off
   setPColor(&strip2,clear_led,clear_bright);
+  setPColor(&strip3,clear_led,clear_bright);
 
-  setPColor(&strip0, c, 50); setPColor(&strip1, a, 50); setPColor(&strip2,b,50);
-  delay(1500);  
+  Serial.println("[5]");
+  setPColor(&strip0, a, 150); setPColor(&strip1, a, 150); setPColor(&strip2,b,150);setPColor(&strip3,c,255);
+  delay(2500);  
   
+  Serial.println("[6]");
   setPColor(&strip0, clear_led, clear_bright);  //Turn Off
   setPColor(&strip1,clear_led,clear_bright);  // Turn Off
   setPColor(&strip2,clear_led,clear_bright);
+  setPColor(&strip3,clear_led,clear_bright);
 
+  Serial.println("[7]");
+  setPColor(&strip0, b, 50); setPColor(&strip1, b, 50); setPColor(&strip2,b,50);setPColor(&strip3,d,255);
+  delay(2500);  
+  
+  Serial.println("[8]");
+  setPColor(&strip0, clear_led, clear_bright);  //Turn Off
+  setPColor(&strip1,clear_led,clear_bright);  // Turn Off
+  setPColor(&strip2,clear_led,clear_bright);
+  setPColor(&strip3,clear_led,clear_bright);
+  
+  Serial.println("[9]");
+  setPColor(&strip0,clear_led,155);
+  setPColor(&strip1,clear_led,155);
+  setPColor(&strip3,e,255);
+  delay(2000);
+    
+  setPColor(&strip0, clear_led, clear_bright);  //Turn Off
+  setPColor(&strip1,clear_led,clear_bright);  // Turn Off
+  setPColor(&strip2,clear_led,clear_bright);
+  setPColor(&strip3,clear_led,clear_bright);
+  
+  Serial.println("[10]");
   rainbow(&strip1, 10);
   rainbow(&strip0, 15);
   rainbow(&strip2,10);
+  rainbow(&strip3,10);
   delay(1000);
 }
 
